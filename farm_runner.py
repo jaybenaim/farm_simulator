437357688aacd4d86a1fb26b0a1ce81655c60e3b
jaybenaim <benaimjacob@gmail.com>
2019-07-22 15:37:55 -0400

    reformatted using black

--- farm_runner.py
+++ farm_runner.py
@@ -1,17 +1,18 @@
-from farm_simulator import * 
+from farm_simulator import *
 import sys
 
-class FARM_RUNNER: 
-    def main_menu(self): 
-        try: 
-            while True: 
+
+class FARM_RUNNER:
+    def main_menu(self):
+        try:
+            while True:
                 self.print_main_menu()
                 user_selected = int(input())
-                self.call_option(user_selected) 
-        except ValueError: 
+                self.call_option(user_selected)
+        except ValueError:
             sys.exit("Goodbye!")
 
-    def print_main_menu(self): 
+    def print_main_menu(self):
         print("[1] Add a new field")
         print("[2] Harvest all fields")
         print("[3] Display a field")
@@ -19,37 +20,38 @@ class FARM_RUNNER:
         print("[5] Exit")
         print("Enter a number: ")
 
-    def call_option(self, user_selected): 
-        if user_selected == 1: 
-            self.add_field() 
-        elif user_selected == 2: 
-            self.harvest_fields() 
-        elif user_selected == 3: 
-            self.display_field() 
-        elif user_selected == 4: 
+    def call_option(self, user_selected):
+        if user_selected == 1:
+            self.add_field()
+        elif user_selected == 2:
+            self.harvest_fields()
+        elif user_selected == 3:
+            self.display_field()
+        elif user_selected == 4:
             self.relax_and_view()
-        elif user_selected == 5: 
-            sys.exit("Goodbye") 
+        elif user_selected == 5:
+            sys.exit("Goodbye")
 
-
-    def add_field(self): 
+    def add_field(self):
         type_of_field = input("Which type of field would you like to add?\n")
-        area_of_field = input("What is the area of the field you are trying to create? \n")
+        area_of_field = input(
+            "What is the area of the field you are trying to create? \n"
+        )
         Farm.field(type_of_field, area_of_field)
 
-    def harvest_fields(self): 
-        print(f'\n{Farm.harvest()}\n') 
-        
-    def display_field(self): 
+    def harvest_fields(self):
+        print(f"\n{Farm.harvest()}\n")
+
+    def display_field(self):
         field = input("Which field would you like to view?\n")
-        print(f'\n{Farm.status(field)}\n')
+        print(f"\n{Farm.status(field)}\n")
 
-    def relax_and_view(self): 
+    def relax_and_view(self):
         print(Farm.relax())
-    
 
-farm = FARM_RUNNER() 
-farm.main_menu() 
 
-# farm.add_field() 
+farm = FARM_RUNNER()
+farm.main_menu()
+
+# farm.add_field()
 # print(Farm.fields)
